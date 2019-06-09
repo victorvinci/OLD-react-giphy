47adb3c9c7ebd41344279ad0cfe90654dcba80d8
victor vinci <victorvinci@protonmail.com>
2019-06-09 19:01:20 +0200

    pass props with the search function to searchBar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -32,7 +32,7 @@ class App extends Component {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchList={this.search}/>
           <div className="selected-gif">
             <Gif id={this.state.selectedGifId}/>
           </div>
