e125cfd7c8615dd8dbd69cb27ac29e2b283854db
victor vinci <victorvinci@protonmail.com>
2019-06-10 00:59:17 +0200

    add function to click and select new gif

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Gif = (props) => {
-  const src = `https://media2.giphy.com/media/${props.id}/200.gif`;
-  return (
-    <img src={src} alt="" className="gif" />
-  );
-};
+class Gif extends Component {
+  handleClick = () => {
+    this.props.selectGif(this.props.id);
+  }
+
+  render() {
+    const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
+    return (
+      <img src={src} alt="" className="gif" onClick={this.handleClick} />
+    );
+  }
+}
 
 export default Gif;
